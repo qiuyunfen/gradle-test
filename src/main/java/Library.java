234a7c60f7ac77bcb7d744353e23059b716eb4cf
qiuyunfen <531802979@qq.com>
2016-12-14 14:43:25 +0800

    here is commit test

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -8,4 +8,7 @@ public class Library {
     public boolean someLibraryMethod() {
         return true;
     }
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
 }
