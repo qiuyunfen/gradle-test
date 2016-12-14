5dc7728b1a3a3054b4127a48a7f29b555e93edb9
qiuyunfen <531802979@qq.com>
2016-12-14 16:31:09 +0800

    junit evenMethod

--- src/test/java/NumberListTest.java
+++ src/test/java/NumberListTest.java
@@ -13,4 +13,13 @@ public class NumberListTest {
         for(int i = 1; i <= 10; i+=2) oddList.add(i);
         assertEquals("oddMethod should return oddList", oddList, numberList.Odd(numberlist));
     }
+    @Test
+    public void testEvenMethod() {
+        NumberList numberList = new NumberList();
+        List<Integer> numberlist = new ArrayList<Integer>();
+        for(int i = 1; i <= 10; i++) numberlist.add(i);
+        List<Integer> evenList = new ArrayList<Integer>();
+        for(int i = 2; i <= 10; i+=2) evenList.add(i);
+        assertEquals("oddMethod should return oddList", evenList, numberList.Even(numberlist));
+    }
 }
